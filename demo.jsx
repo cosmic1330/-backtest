dd52e56c2642667cf6a4b6949b991997ee752c94
楊竣宇 <yangjunyu@yangjunyudeMacBook-Air.local>
2023-02-22 22:52:48 +0800

    add buy williams

--- demo.jsx
+++ demo.jsx
@@ -12,42 +12,58 @@ function customBuyMethod(data) {
   let macd = new Macd();
   let rsi = new Rsi();
   let kd = new Kd();
-  const Ema26 = macd.getEMA26(data);
-  const Ema12 = macd.getEMA12(data);
-  const Dif = macd.getDIF(data, Ema12, Ema26);
-  const Macd9 = macd.getMACD9(data, Dif);
-
-  let ma10Data = ma.getMa10(data);
-  let ma5Data = ma.getMa5(data);
-  let ma20Data = ma.getMa20(data);
-  let ma60Data = ma.getMa60(data);
+  let williams = new Williams();
+  // m1
   let rsiData = rsi.getAllRsi(data);
-  let kdData = kd.getKD(data);
+  let williamsData = williams.getAllWillams(data);
+  // m2
+  // const Ema26 = macd.getEMA26(data);
+  // const Ema12 = macd.getEMA12(data);
+  // const Dif = macd.getDIF(data, Ema12, Ema26);
+  // const Macd9 = macd.getMACD9(data, Dif);
+
+  // let ma10Data = ma.getMa10(data);
+  // let ma5Data = ma.getMa5(data);
+  // let ma20Data = ma.getMa20(data);
+  // let ma60Data = ma.getMa60(data);
+  // let rsiData = rsi.getAllRsi(data);
+  // let kdData = kd.getKD(data);
   let res = {
     status: false,
     detail: "Not up to standard",
   };
   if (
-    data[data.length - 1]["v"] > 1500 &&
-    data[data.length - 2]["v"] > 1500 &&
-    kdData[data.length - 1]["k-d"] > 3 &&
-    kdData[data.length - 1]["k"] > 50 &&
-    kdData[data.length - 1]["k"] > kdData[data.length - 1]["d"] &&
-    kdData[data.length - 2]["k"] < kdData[data.length - 2]["d"] &&
-    Dif[data.length - 1]["DIF"] > 0 &&
-    Macd9[data.length - 1]["MACD9"] > 0 &&
-    Macd9[data.length - 1]["OSC"] > Macd9[data.length - 2]["OSC"] &&
-    Macd9[data.length - 1]["OSC"] > Macd9[data.length - 3]["OSC"] &&
-    Macd9[data.length - 1]["OSC"] > Macd9[data.length - 4]["OSC"] &&
-    data[data.length - 1]["c"] > ma10Data[ma10Data.length - 1]["ma10"] &&
-    ma5Data[ma5Data.length - 1]["ma5"] >
-      ma20Data[ma20Data.length - 1]["ma20"] &&
-    ma20Data[ma20Data.length - 1]["ma20"] >
-      ma60Data[ma60Data.length - 1]["ma60"] &&
-    ma5Data[ma5Data.length - 1]["ma5"] >
-      ma60Data[ma60Data.length - 1]["ma60"] &&
-    rsiData[rsiData.length - 1]["rsi6"] < 75 &&
-    rsiData[rsiData.length - 1]["rsi6"] > 30
+    // m1:
+    (williamsData[williamsData.length - 2].williams9 < -80 ||
+      williamsData[williamsData.length - 3].williams9 < -80||
+      williamsData[williamsData.length - 4].williams9 < -80) &&
+    (williamsData[williamsData.length - 2].williams18 < -80 ||
+      williamsData[williamsData.length - 3].williams18 < -80||
+      williamsData[williamsData.length - 4].williams9 < -80) &&
+      (rsiData[rsiData.length - 1].rsi6>rsiData[rsiData.length - 1].rsi12) &&
+      (rsiData[rsiData.length - 2].rsi6<rsiData[rsiData.length - 2].rsi12)
+
+    // m2:
+    // data[data.length - 1]["v"] > 1500 &&
+    // data[data.length - 2]["v"] > 1500 &&
+    // kdData[data.length - 1]["k-d"] > 3 &&
+    // kdData[data.length - 1]["k"] > 50 &&
+    // kdData[data.length - 1]["k"] > kdData[data.length - 1]["d"] &&
+    // kdData[data.length - 2]["k"] < kdData[data.length - 2]["d"] &&
+    // Dif[data.length - 1]["DIF"] > 0 &&
+    // Macd9[data.length - 1]["MACD9"] > 0 &&
+    // Macd9[data.length - 1]["OSC"] > Macd9[data.length - 2]["OSC"] &&
+    // Macd9[data.length - 1]["OSC"] > Macd9[data.length - 3]["OSC"] &&
+    // Macd9[data.length - 1]["OSC"] > Macd9[data.length - 4]["OSC"] &&
+    // data[data.length - 1]["c"] > ma10Data[ma10Data.length - 1]["ma10"] &&
+    // ma5Data[ma5Data.length - 1]["ma5"] >
+    //   ma20Data[ma20Data.length - 1]["ma20"] &&
+    // ma20Data[ma20Data.length - 1]["ma20"] >
+    //   ma60Data[ma60Data.length - 1]["ma60"] &&
+    // ma5Data[ma5Data.length - 1]["ma5"] >
+    //   ma60Data[ma60Data.length - 1]["ma60"] &&
+    // rsiData[rsiData.length - 1]["rsi6"] < 75 &&
+    // rsiData[rsiData.length - 1]["rsi6"] > 30
   ) {
     res.status = true;
     res.detail = `no message`;
@@ -98,9 +114,9 @@ function App() {
   const handleClick = () => {
     for (let i = 0; i < 100; i++) {
       context.current.run();
-      console.log(context.current.dateSequence.currentDate)
+      console.log(context.current.dateSequence.currentDate);
     }
-    console.log("end")
+    console.log("end");
     setCapital(context.current.capital);
     setProfit(context.current.record.profit);
     setWin(context.current.record.win);
